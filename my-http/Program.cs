7050c8e7b98a904255272275400055cdf7635039
NikitinRadmir <145252092+NikitinRadmir@users.noreply.github.com>
2025-01-19 00:15:47 +0300

    Update Program.cs

--- my-http/Program.cs
+++ my-http/Program.cs
@@ -19,8 +19,23 @@ class Program
     {
         
         var config = AppConfig.GetInstance(); // инициализируем AppConfig
+        var connectionString = config.ConnectionStrings["DefaultConnection"];
+
+        try
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Подключение к базе данных успешно!");
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Ошибка подключения к базе данных: {ex.Message}");
+        }
         var prefixes = new[] { $"http://{config.Domain}:{config.Port}/" }; // Собираем префиксы в список
         var server = new HttpServerWithTypes(prefixes, config.Path); // создаем сервер с префиксами
+        Console.WriteLine("АДРЕС СВЕРХУ НЕДЕЙСТВИТЕЛЕН.\nВОТ ПРАВИЛЬНЫЙ: http://localhost:8888/");
         server.Start(); // запускаем сервер
     }
 
